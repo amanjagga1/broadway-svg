0e1d1595a165fec5943acb7caf13a57573828927
manasKumar26 <manas.kumar@headout.com>
2024-07-03 20:14:10 +0530

    feat: parser

---
+++ input.py
@@ -0,0 +1,9 @@
+subsection_strings = [
+    'Rear Balcony',
+    'Front Balcony',
+    'Rear Mezzanine',
+    'Orchestra far sides',
+    'Mid mezzanine',
+    'Front orchestra sides',
+    'Orchestra Center',
+]
